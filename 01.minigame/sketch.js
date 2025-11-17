2b0751c03cd084d7a52102b1a0a29b0a1373a6a5
axolotl-teddy-bear <wonginseong@gmail.com>
2025-11-17 18:10:25 +0000

    edit

--- 01.minigame/sketch.js
+++ 01.minigame/sketch.js
@@ -15,74 +15,13 @@ function preload() {
 
 function setup() {
   createCanvas(1000, 750);
-  let obj_amt = floor(random(5, 12));
-  for (let i = 0; i < obj_amt; i++) {   //generate the amount of objs needed
+  let obj_num = floor(random(5, 12));
+  for (let i = 0; i < obj_num; i++) {   //generate the amount of objs needed
     gen_obj(); // generate each object up to selected amount
   //When I was trying to make the objects draggablet, I found the process to be very confusing because my objects are stored in an array. to use the draggable() function (as seen in the ma1805 repository examples) i need an element that is an object.
    shapes = new Draggable(obj_pos[i].x, obj_pos[i].y, 50, 50);
   }
 }
-
-//This code references the 'click and drag' example taken from the ma1805 code repository. 
-
-class Draggable {
-    constructor(x, y, w, h) {
-      this.dragging = false; // Is the object being dragged?
-      this.rollover = false; // Is the mouse over the ellipse?
-      this.x = x;
-      this.y = y;
-      this.w = w;
-      this.h = h;
-      this.offsetX = 0;
-      this.offsetY = 0;
-    }
-  
-    over() {
-      // Is mouse over object
-      if (mouseX > this.x && mouseX < this.x + this.w && mouseY > this.y && mouseY < this.y + this.h) {
-        this.rollover = true;
-      } else {
-        this.rollover = false;
-      }
-    }
-  
-    update() {
-      // Adjust location if being dragged
-      if (this.dragging) {
-        this.x = mouseX + this.offsetX;
-        this.y = mouseY + this.offsetY;
-      }
-    }
-  
-    show() {
-      stroke(0);
-      image(asset4, this.x, this.y, 50, 50);
-      
-      if (this.dragging) {
-        fill(50);
-      } else if (this.rollover) {
-        fill(100);
-      } else {
-        fill(175, 200);
-      }
-      rect(this.x, this.y, this.w, this.h);
-    }
-  
-    pressed() {
-      // Did I click on the rectangle?
-      if (mouseX > this.x && mouseX < this.x + this.w && mouseY > this.y && mouseY < this.y + this.h) {
-        this.dragging = true;
-        // If so, keep track of relative location of click to corner of rectangle
-        this.offsetX = this.x - mouseX;
-        this.offsetY = this.y - mouseY;
-      }
-    }
-  
-    released() {
-      // Quit dragging
-      this.dragging = false;
-    }
-  }
 
 function draw() {
   background(220);
@@ -92,14 +31,26 @@ function draw() {
   //circle(500, 375, 400);
 
   //the section below that draws the objs are based off of the 03.multiples project i did for the last assignment. 
+
+  for (let obj of obj_pos) { //drawing objs
+    if (obj.amt == 1) {
+      image(asset4, obj.x, obj.y, 50, 50);
+    } else if (obj.amt == 2) {
+      image(asset5, obj.x, obj.y, 50, 50);
+    } else if (obj.amt == 3) {
+      image(asset6, obj.x, obj.y, 50, 50);
+    } else if (obj.amt == 4) {
+      image(asset7, obj.x, obj.y, 50, 50);
+    } else if (obj.amt == 5) {
+      image(asset8, obj.x, obj.y, 50, 50);
+    }
+
     shapes.over();
     shapes.update();
     shapes.show();
   }
 
   //makes the image change when the mouse is pressed
-  
-  /*
   fill(255);
   if (mouseIsPressed) {
     push();
@@ -112,11 +63,9 @@ function draw() {
     image(asset3, mouseX, mouseY, 400, 500)
     pop();
   }
-  */
-
 
   
-
+}
 
 function mousePressed() {
   shapes.pressed();
@@ -129,6 +78,6 @@ function mouseReleased() {
 function gen_obj() { //generates random position then adds then into the array
   x_pos = random(320, 650);
   y_pos = random(200, 525);
-  obj_num = floor(random(1, 6));
-  obj_pos.push({ x: x_pos, y: y_pos, num: obj_num }); 
+  obj_amt = floor(random(1, 6));
+  obj_pos.push({ x: x_pos, y: y_pos, amt: obj_amt }); //makes the objs in the array draggables
 }
