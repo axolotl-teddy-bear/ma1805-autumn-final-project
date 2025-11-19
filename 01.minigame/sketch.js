3d89a92a27dfa44178f8e0e6f2b1f24bc86be470
Mikaela Wong <wonginseong@gmail.com>
2025-11-19 23:58:49 +0000

    edit

--- 01.minigame/sketch.js
+++ 01.minigame/sketch.js
@@ -3,9 +3,9 @@ let y_pos;
 let obj_pos = [];
 let shapes = [];
 
-let sec_limit = 20;
+let sec_limit = 22;
 let sec_left;
-let milis_limit = 20000; //1000milis is a second
+let milis_limit = 22000; //1000milis is a second
 
 function preload() {
   asset1 = loadImage("assets/images/bg.PNG");
@@ -23,7 +23,7 @@ function preload() {
 
 function setup() {
   createCanvas(1000, 750);
-  let obj_num = floor(random(10, 20));
+  let obj_num = floor(random(15, 25));
   for (let i = 0; i < obj_num; i++) {   //generate the amount of objs needed
     gen_obj(); // generate each object up to selected amount
   //When I was trying to make the objects draggablet, I found the process to be very confusing because my objects are stored in an array. to use the draggable() function (as seen in the ma1805 repository examples) i need an element that is an object.
@@ -121,6 +121,8 @@ function getLeafAsset(amount) {
   }
 }
 
+//ENDGAME
+let hasPlayedSound = false; 
 function endGame() {
   fill(127, 127)
   rect(0, 0, 1000, 750)
@@ -128,5 +130,10 @@ function endGame() {
   textAlign(CENTER)
   textSize(100);
   text("You lose :(", 500, 375)
-  sound2.play();
+    if (hasPlayedSound === false) {
+    sound2.play(); // Play the sound
+    hasPlayedSound = true; // Set flag to true to prevent replay
+  }
+
+  endGame();
 }
